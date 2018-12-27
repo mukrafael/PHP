995e16fcabfead4e27192edbfcd0fdc9866f8d92
Your Name <you@example.com>
2018-12-27 11:00:26 -0200

    {AULA 05} Operadores php

---
+++ Operadores.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8"/>
+	<title></title>
+</head>
+<body>
+	<?php
+	$n1 = $_GET["a"];
+	$n2 = $_GET["b"];
+	echo "<h2>Valores recebidos: $n1 e $n2</h2>";
+	$m = ($n1 + $n2) / 2;
+	$s = $n1 + $n2;
+	echo "A soma entre $n1 + $n2 é igual $s";
+	echo "</br>A soma vale ". ($n1+$n2);
+	echo "</br>A subtracao vale ". ($n1-$n2);
+	echo "</br>A multiplicacao vale ". ($n1*$n2);
+	echo "</br>A divisao vale ". ($n1/$n2);
+	echo "</br>O modulo vale ". ($n1%$n2);
+	echo "</br>A média vale $m";
+	?>
+</body>
+</html>
