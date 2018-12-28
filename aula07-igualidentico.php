070a127fc60c34d6e0373f728a3d7faa4a1feea5
Your Name <you@example.com>
2018-12-28 11:00:27 -0200

    {Aula 07} igual identico em php

---
+++ aula07-igualidentico.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br>
+<head>
+	<meta charset="UTF-8"/>
+	<link rel="stylesheet" href="_css/estilo.css"/>
+	<title> Operadores Relacionais</title>
+</head>
+<body>
+<div>
+	<?php
+		$a = 3;
+		$b = "3";
+		$r = $a == $b ? "SIM" : "NÂO";
+		echo "As variaveis A e B sao iguais? $r";
+	?>
+</div>	
+</body>
+</html>
